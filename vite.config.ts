d25de6615272b6c9c7fc0c17b9266150b2285bb1
Andrey Filyanin <filyaniny@gmail.com>
2024-11-19 23:23:57 +0400

    v0.0.17 CI/CD working version #1

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,20 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
 import basicSsl from '@vitejs/plugin-basic-ssl';
+import { nodePolyfills } from 'vite-plugin-node-polyfills'
+import tsconfigPaths from 'vite-tsconfig-paths';
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), basicSsl()],
+  plugins: [
+    react(),
+    tsconfigPaths(),
+    basicSsl(),
+    nodePolyfills({
+        globals: {
+            Buffer: true
+        }
+    })],
   build: {
     outDir: './dist'
   },
